e4b0842660a04f454cd22253c9a3ab6efe2b0a10
hembram5 <ganeshhembram5@gmail.com>
2022-09-05 20:29:26 +0530

    initial development

---
+++ force-app/main/default/lwc/sObjectContainer/sObjectContainer.js
@@ -0,0 +1,24 @@
+/**
+ * description: To import a class, function, or variable declared in a module, use the import statement
+ */
+import { LightningElement } from 'lwc';
+import getSObjectList from '@salesforce/apex/sObjectController.getSObject';
+
+/**
+ * description: To allow other code to use a class, function, or variable declared in a module, use the export statement
+ */
+export default class SObjectContainer extends LightningElement {
+    
+    /**
+     * description: The connectedCallback invoke when a component is inserted into the DOM. This call the Apex method to reterive list of sObject
+     */
+    connectedCallback() {
+        getSObjectList()
+        .then(result => {
+            console.log('Data Check ==> '+JSON.parse(result));
+        })
+        .catch(error => {
+
+        })
+    }
+}
