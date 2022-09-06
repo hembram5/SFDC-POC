068cfe6212db7099fd2ceac0114c102b899679ff
hembram5 <ganeshhembram5@gmail.com>
2022-09-06 12:44:33 +0530

    Updated Code

--- force-app/main/default/lwc/sObjectContainer/sObjectContainer.js
+++ force-app/main/default/lwc/sObjectContainer/sObjectContainer.js
@@ -1,13 +1,15 @@
 /**
  * description: To import a class, function, or variable declared in a module, use the import statement
  */
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import getSObjectList from '@salesforce/apex/sObjectController.getSObject';
 
 /**
  * description: To allow other code to use a class, function, or variable declared in a module, use the export statement
  */
 export default class SObjectContainer extends LightningElement {
+
+    @track objectMap = [];
     
     /**
      * description: The connectedCallback invoke when a component is inserted into the DOM. This call the Apex method to reterive list of sObject
@@ -15,10 +17,16 @@ export default class SObjectContainer extends LightningElement {
     connectedCallback() {
         getSObjectList()
         .then(result => {
-            console.log('Data Check ==> '+JSON.stringify(result));
+            //console.log(JSON.stringify(result));
+            this.objectMap = Object.entries(result).map(([value, label]) => ({ value, label }));
+            //console.log('OUTPUT Map : '+JSON.stringify(this.objectMap));
         })
         .catch(error => {
-
+            console.log('OUTPUT : '+ JSON.stringify(error));
         })
+    }
+
+    handleSeletedObject(event) {
+        console.log('Selected Value : '+event.detail.value);
     }
 }
